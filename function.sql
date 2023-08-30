01377c5d46c13b79c430f1a4dad1171e8d8e571e
Ornella Sereno Alloa <90015172+Ornella07@users.noreply.github.com>
2023-08-30 11:32:08 -0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -19,4 +19,23 @@ END;
 
 
 
+DELIMITER //
 
+CREATE FUNCTION obtener_actuaciones_de_actor(id_actor_param INT) RETURNS TEXT
+DETERMINISTIC
+BEGIN
+    DECLARE actua TEXT;
+    
+    SELECT GROUP_CONCAT(DISTINCT
+        CONCAT('Serie: ', s.nombre, ', Película: ', p.nombre) SEPARATOR '; ') INTO actua
+    FROM actores a
+    INNER JOIN actua act ON a.id_actor = act.id_actor
+    INNER JOIN pelicula p ON act.id_pelicula = p.id_pelicula
+    LEFT JOIN serie s ON p.id_serie = s.id_serie
+    WHERE a.id_actor = id_actor_param;
+    
+    RETURN actua;
+END;
+//
+
+DELIMITER ;
